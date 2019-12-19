059e9214a8437448f3faca81028702dd6b77dd6a
lgv-0 <55011072+lgv-0@users.noreply.github.com>
2019-12-19 15:35:10 -0500

    Student class

--- index.js
+++ index.js
@@ -190,8 +190,36 @@ class Instructor extends Lambdasian
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
+class Student extends Lambdasian
+{
+  constructor(a_Args)
+  {
+    super(a_Args);
+    this.previousBackground = a_Args["previousBackground"];
+    this.className = a_Args["className"];
+    this.favSubjects = a_Args["favSubjects"];
+  }
 
+  listSubjects()
+  {
+    let s_Subs = "";
+    this.favSubjects.forEach(i =>
+      {
+        s_Subs += i + ", ";
+      });
+    
+    return s_Subs;
+  }
+
+  PRAssignment(s_Subject)
+  {
+    return `${this.name} has submitted a PR for ${s_Subject}`;
+  }
+
+  sprintChallenge(s_Subject)
+  {
+    return `${this.name} has begun sprint challenge on ${s_Subject}`;
+  }
 }
 
 /*
